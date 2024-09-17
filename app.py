c6a114b9b254724c565882241a864d1347517a96
Angus Clayton <clayton.angus@outlook.com>
2024-09-17 22:35:32 +1000

    update db file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import traceback
 
 # create db connection
 def get_db_connection():
-    conn = sqlite3.connect('test.db')
+    conn = sqlite3.connect('/var/www/F1_LEADERBOARD/test.db')
     conn.row_factory = sqlite3.Row  # Set row_factory to sqlite3.Row
     cursor = conn.cursor()
     return conn, cursor
